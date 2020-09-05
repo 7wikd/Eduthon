ab4af5b5c636ee8dba1f3710dc227c21a2388b66
Aditya Vadalkar <adityavadalkar@gmail.com>
2020-09-05 19:24:57 +0530

    Added trigger

---
+++ Assets/Scripts/Stack.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stack : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] stackbox;
+}
